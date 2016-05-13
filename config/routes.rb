d6a37ecb4f45d53f0c07ae343eb5f2a31a0f06fb
Ngoc Nguyen <nguyenngoc2505@gmail.com>
2016-05-13 15:40:43 +0700

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   get "profile", to: "profile#index"
-  resources :articles, path: "blogs" do
-    post :upload, on: :member
-  end
+  resources :articles, path: "blogs"
   get "/:id", to: "articles#show"
 end
